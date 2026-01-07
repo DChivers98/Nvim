849cf46ac27cd5d17f397ddd6aeaf8773ad454a5
DChivers98 <91552728+DChivers98@users.noreply.github.com>
2026-01-07 14:42:13 +0000

    Add div surround command

--- lua/plugins/surround.lua
+++ lua/plugins/surround.lua
@@ -1,7 +1,18 @@
 return {
-  "kylechui/nvim-surround",
-  event = "VeryLazy",
-  config = function()
-    require("nvim-surround").setup({})
-  end,
+    "kylechui/nvim-surround",
+    event = "VeryLazy",
+    config = function()
+        require("nvim-surround").setup({
+            surrounds = {
+                ["D"] = {
+                    add = { "<div>", "</div>" },
+                },
+            },
+            keymaps = {
+                normal = "ys",
+                normal_cur = "yssiw",
+                normal_line = "yS",
+            },
+        })
+    end,
 }
