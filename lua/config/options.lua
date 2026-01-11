321241738205c2b826b761e54c90a8cebe166a83
DChivers98 <91552728+DChivers98@users.noreply.github.com>
2026-01-11 18:42:40 +0000

    Removed extras

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,8 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.g.lazyvim_php_lsp = "phpactor"
-vim.g.intelephense_enabled = false
+vim.g.lazyvim_php_lsp = "intelephense"
 
 local opt = vim.opt
 
